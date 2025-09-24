ca957dfa857b3c4727fa8b3db9e77f1f3a96cc2c
SousiOmine <110832262+SousiOmine@users.noreply.github.com>
2025-09-24 20:41:03 +0900

    参照を追加　local.props.templateのMetasiaApplicationPathをMetasiaの実行ファイルがあるフォルダにしてね

---
+++ FFmpegPlugin/FFmpegPlugin/FFmpegPlugin.cs
@@ -0,0 +1,24 @@
+﻿using Metasia.Editor.Plugin;
+
+namespace Metasia.Editor.FFmpegPlugin;
+
+public class FFmpegPlugin : IEditorPlugin
+{
+    public string PluginIdentifier { get; } = "SousiOmine.FFmpegPlugin";
+    public string PluginVersion { get; } = "0.0.1";
+    public string PluginName { get; } = "FFmpegInput&Output";
+    public IEnumerable<IEditorPlugin.SupportEnvironment> SupportedEnvironments { get; } = new[]
+    {
+        IEditorPlugin.SupportEnvironment.Windows_arm64,
+        IEditorPlugin.SupportEnvironment.Windows_x64,
+        IEditorPlugin.SupportEnvironment.MacOS_arm64,
+        IEditorPlugin.SupportEnvironment.MacOS_x64,
+        IEditorPlugin.SupportEnvironment.Linux_arm64,
+        IEditorPlugin.SupportEnvironment.Linux_x64,
+    };
+    
+    public void Initialize()
+    {
+        Console.WriteLine("Hello! from FFmpeg Plugin!");
+    }
+}
